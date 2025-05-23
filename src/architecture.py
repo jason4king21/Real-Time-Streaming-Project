62935c41fcebceeae024212aeef73a4157a38619
jason4king21 <jason4king21@gmail.com>
2025-05-23 16:01:21 -0500

    commit

--- src/architecture.py
+++ src/architecture.py
@@ -1,15 +1,27 @@
-from diagrams import Diagram
+from diagrams import Diagram, Node
 from diagrams.aws.network import APIGateway
 from diagrams.aws.compute import Lambda
 from diagrams.aws.analytics import KinesisDataFirehose
 from diagrams.aws.storage import S3
-from diagrams.generic.database import SQL  # Placeholder for Snowflake
 
-with Diagram("Real-Time Streaming Pipeline", show=False, filename="diagrams/architecture", outformat="png"):
+class CustomSnowflake(Node):
+    _provider = "custom"
+    _icon_dir = "../resources"  # One level up from src/
+
+    # fontcolor = "#ffffff"
+    _dot_attrs = {
+        "shape": "none",
+        "xlabel": "Snowflake"
+    }
+
+    def __init__(self, label="snowflake"):
+        super().__init__(label)
+
+with Diagram("Real-Time Streaming Pipeline", show=False, filename="../diagrams/architecture", outformat="png"):
     api = APIGateway("API Gateway")
     lam = Lambda("Lambda Processor")
     firehose = KinesisDataFirehose("Firehose")
     s3 = S3("S3 Bucket")
-    snowflake = SQL("Snowflake")
+    snowflake = CustomSnowflake("snowflake")
 
     api >> lam >> firehose >> s3 >> snowflake
