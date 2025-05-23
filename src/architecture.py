5755bee368d5a60a2357b6e4cfc295614e2e3ec2
jason4king21 <jason4king21@gmail.com>
2025-05-23 17:12:53 -0500

    diagrams

--- src/architecture.py
+++ src/architecture.py
@@ -3,25 +3,17 @@ from diagrams.aws.network import APIGateway
 from diagrams.aws.compute import Lambda
 from diagrams.aws.analytics import KinesisDataFirehose
 from diagrams.aws.storage import S3
+from diagrams.saas.analytics import Snowflake
 
-class CustomSnowflake(Node):
-    _provider = "custom"
-    _icon_dir = "../resources"  # One level up from src/
 
-    # fontcolor = "#ffffff"
-    _dot_attrs = {
-        "shape": "none",
-        "xlabel": "Snowflake"
-    }
-
-    def __init__(self, label="snowflake"):
-        super().__init__(label)
 
 with Diagram("Real-Time Streaming Pipeline", show=False, filename="../diagrams/architecture", outformat="png"):
     api = APIGateway("API Gateway")
     lam = Lambda("Lambda Processor")
     firehose = KinesisDataFirehose("Firehose")
     s3 = S3("S3 Bucket")
-    snowflake = CustomSnowflake("snowflake")
+    s3error = S3("Error Logs")
+    snowflake = Snowflake("Snowflake")
 
     api >> lam >> firehose >> s3 >> snowflake
+    lam >> s3error
